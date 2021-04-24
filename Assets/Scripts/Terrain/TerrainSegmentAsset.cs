f16dcd46fa290873178f4747f4e721bb30a491f7
Jaoel <josv.svensson@gmail.com>
2021-04-24 15:29:53 +0200

    Add huge shaft

---
+++ Assets/Scripts/Terrain/TerrainSegmentAsset.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace LD48 {
+    [CreateAssetMenu(fileName = "New Terrain Segment", menuName = "LD48/Terrain Segment")]
+    public class TerrainSegmentAsset : ScriptableObject {
+        public TileSegment tilePrefab = null;
+        public ShaftSegment shaftPrefab = null;
+    }
+}
