b4e783c0868a1c5512c03ff8f095c29cac4e08b8
Bentebent <henriksson.tim@gmail.com>
2021-04-24 18:09:18 +0200

    WIP probe

---
+++ Assets/Scripts/Entities/ProbeStation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD48 {
+    public class ProbeStation : Interactable {
+        private void Awake() {
+
+        }
+
+        private void Start() {
+
+        }
+
+        protected override void Update() {
+            base.Update();
+        }
+    }
+}
