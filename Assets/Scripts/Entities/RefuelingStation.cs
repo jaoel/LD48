ffb8b044a803346e9f9573c56759dfaea4845935
Jaoel <josv.svensson@gmail.com>
2021-04-26 21:04:55 +0200

    Add fuel sound

--- Assets/Scripts/Entities/RefuelingStation.cs
+++ Assets/Scripts/Entities/RefuelingStation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace LD48 {
@@ -7,6 +5,15 @@ namespace LD48 {
 
         [SerializeField]
         private Transform _toolTipPos = null;
+
+        public AudioSource audioSource;
+
+        private float audioAmount = 0f;
+
+        private void Start() {
+            audioSource.Play();
+            audioSource.Pause();
+        }
 
         protected override void OnInteract() {
             base.OnInteract();
@@ -20,6 +27,19 @@ namespace LD48 {
 
         protected override void Update() {
             base.Update();
+
+            if (Interacting) {
+                audioAmount = Mathf.MoveTowards(audioAmount, 1f, Time.deltaTime * 2f);
+            } else {
+                audioAmount = Mathf.MoveTowards(audioAmount, 0f, Time.deltaTime * 2f);
+            }
+
+            if (audioAmount == 0f) {
+                audioSource.Pause();
+            } else {
+                audioSource.UnPause();
+                audioSource.pitch = Mathf.Lerp(0.4f, 1f, audioAmount);
+            }
 
             if (PlayerInReach) {
                 UIManager.Instance.DisplayTextPanel(_toolTipPos, "Hold [space] to refuel");
