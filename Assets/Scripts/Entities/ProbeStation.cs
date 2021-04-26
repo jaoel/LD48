c26851446b0506f457c40d090f67318761ab2888
Jaoel <josv.svensson@gmail.com>
2021-04-26 13:26:55 +0200

    Add probe station

--- Assets/Scripts/Entities/ProbeStation.cs
+++ Assets/Scripts/Entities/ProbeStation.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 namespace LD48 {
     public class ProbeStation : Interactable {
+        private const float leverOnAngle = 90f;
+        private const float leverOffAngle = 0f;
+
+        public Transform leverArm;
+
+        private float leverCurrentAngle = 0f;
 
         [SerializeField]
         private Probe _probe = null;
 
-        private void Awake() {
-
-        }
-
-        private void Start() {
-
-        }
-
         protected override void Update() {
             base.Update();
+
+            if (_probe.CurrentState == Probe.State.Idle) {
+                leverCurrentAngle = Mathf.MoveTowards(leverCurrentAngle, leverOffAngle, Time.deltaTime * 90f);
+            } else {
+                float dist = Mathf.Clamp01(Mathf.Abs(leverCurrentAngle - leverOnAngle) / leverOnAngle);
+                dist = dist * dist;
+                leverCurrentAngle = Mathf.MoveTowards(leverCurrentAngle, leverOnAngle, Time.deltaTime * (20f + (1f - dist) * 1000f));
+            }
+            leverArm.localRotation = Quaternion.Euler(leverCurrentAngle, 0f, 0f);
         }
 
         protected override void OnInteract() {
