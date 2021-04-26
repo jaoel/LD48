0d0f66e4077886258206f5823abedd5ee31352d5
Jaoel <josv.svensson@gmail.com>
2021-04-26 19:32:39 +0200

    Add models for stations

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LD48 {
+    public class Spinner : MonoBehaviour {
+        float rot = 0f;
+        private void Awake() {
+            rot = Random.Range(0f, 360f);
+        }
+
+        private void Update() {
+            rot += Time.deltaTime * 35f;
+            transform.localEulerAngles = new Vector3(0f, rot, 0f);
+        }
+    }
+}
