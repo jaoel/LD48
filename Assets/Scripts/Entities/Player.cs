a51990521027436ea15a80329c40020620eddb42
Bentebent <henriksson.tim@gmail.com>
2021-04-26 18:20:41 +0200

    acceleration

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -14,7 +14,10 @@ namespace LD48 {
 
         private float _maxSpeed = 10.0f;
         public float _speed = 7.0f;
-        private float _acceleration = 0.0f;
+        private float _acceleration = 20.0f;
+        private float _currentVelocity = 0;
+
+        private Vector3 _oldForward = Vector3.zero;
         public bool hasDiamond = false;
 
         public int Resources { get; set; }
@@ -43,11 +46,10 @@ namespace LD48 {
             if (_characterController != null) {
 
                 Vector3 forward = Vector3.zero;
-                Vector3 velocity = new Vector3(_speed, 9.82f, _speed);
+
                 if (Input.GetKey(KeyCode.D)) {
                     forward.x = -1;
-                }
-                else if (Input.GetKey(KeyCode.A)) {
+                } else if (Input.GetKey(KeyCode.A)) {
                     forward.x = 1;
                 }
 
@@ -58,21 +60,48 @@ namespace LD48 {
                 }
 
 
-                if (forward != Vector3.zero || !_characterController.isGrounded) {
+                if (forward != Vector3.zero/* || !_characterController.isGrounded*/) {
                     forward = forward.normalized;
 
                     if (forward.x != 0 || forward.z != 0) {
                         transform.forward = forward;
                     }
 
-                    if (!_characterController.isGrounded) {
-                        forward.y = -1.0f;
-                    }
+                    //if (!_characterController.isGrounded) {
+                    //    forward.y = -1.0f;
+                    //}
+
+                    _currentVelocity += _acceleration * Time.deltaTime;
+                    _currentVelocity = Mathf.Min(_currentVelocity, _maxSpeed);
+                    Vector3 velocity = new Vector3(_currentVelocity, 0, _currentVelocity);
 
                     _characterController.Move(Vector3.Scale(forward.normalized, velocity * Time.deltaTime));
+                    _oldForward = forward;
                 }
 
-                animator.SetFloat("RunSpeed", forward.magnitude);
+                if (!_characterController.isGrounded) {
+                    _characterController.Move(new Vector3(0, -1 * 9.82f * Time.deltaTime, 0));
+                }
+
+                if (forward.x == 0 && forward.z == 0 && _currentVelocity > 0) {
+                    _currentVelocity -= _acceleration * Time.deltaTime;
+                    _currentVelocity = Mathf.Max(_currentVelocity, 0);
+                    Vector3 velocity = new Vector3(_currentVelocity, 0, _currentVelocity);
+
+                    _characterController.Move(Vector3.Scale(_oldForward.normalized, velocity * Time.deltaTime));
+
+                    if (_currentVelocity <= 0.0f) {
+                        _oldForward = Vector3.zero;
+                    }
+
+                    animator.SetFloat("RunSpeed", _oldForward.magnitude);
+
+                    Debug.Log(_currentVelocity);
+                } else {
+                    animator.SetFloat("RunSpeed", forward.magnitude);
+                }
+
+                animator.SetFloat("RunSpeed2", Mathf.Max(_currentVelocity / _maxSpeed, 0.1f));
             }
 
             UIManager.Instance.resourcesText.SetText(Resources.ToString() + "/" + MaxResources.ToString());
